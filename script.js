b3aa2097b4c8c61d18b361b23dd9f51981a36f7f
Alexandra List <alexandralist@Alexandras-MacBook-Pro.local>
2022-02-17 18:43:17 -0800

    Created Question Class

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+   console.log("javascriptloaded")
+class Question {
+    questionText = ""
+    answer = false
+    constructor(answer, question){
+        this.questionText = question
+        this.answer = answer
+    }
+}
+// let question1 = new Question(true, "Is George Cool?")
+// console.log(question1)
+// console.log(question1.answer == false)
 
+let questionList =[new Question(true, "Is George Cool?"),new Question(false, "Is George Lame?")]
+for (let index = 0; index < questionList.length; index++) {
+console.log(questionList[index])
+
+
+    
+}
