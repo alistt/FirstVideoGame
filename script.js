8896e99dcdbbd6815788112d804b2cad49a15bf1
Alexandra List <alexandralist@Alexandras-MacBook-Pro.local>
2022-02-19 14:01:43 -0800

    Adding User Info

--- script.js
+++ script.js
@@ -11,7 +11,12 @@ class Question {
 // console.log(question1)
 // console.log(question1.answer == false)
 
-let questionList =[new Question(true, "Is George Cool?"),new Question(false, "Is George Lame?")]
+let questionList =[new Question(true, "Airedale Terriers Served In World Wars"),
+new Question(false, "Airedale Terriers Are Primarily White"),
+new Question(true, "Airedale Terriers Were Bred To Hunt Feasants"),
+new Question(true, "Airedale Terriers Wore Gas Masks In The War"),
+new Question(true, "An Airedale named Bob Received A Victorian Medal"),
+new Question(false, "Airedale Terriers Are Known As The Golden Terrier")]
 for (let index = 0; index < questionList.length; index++) {
 console.log(questionList[index])
 
