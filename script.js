386da8d8e817ee1c1c0f99fd20574a0e4b7eabd3
Alexandra List <alexandralist@Alexandras-MacBook-Pro-2.local>
2022-02-21 15:05:59 -0800

    Added CSS

--- script.js
+++ script.js
@@ -1,25 +1,94 @@
-   console.log("javascriptloaded")
-class Question {
-    questionText = ""
-    answer = false
-    constructor(answer, question){
-        this.questionText = question
-        this.answer = answer
+const startButton= document.getElementById('start-btn')
+const nextButton= document.getElementById('next-btn')
+const questionContainerElement = document.getElementById
+('question-container')
+const questionElement = document.getElementById('question')
+const trueButtonElement = document.getElementById
+('true-btn')
+const falseButtonElement = document.getElementById
+('false-btn')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+ 
+ function startGame(){
+ 
+     startButton.classList.add('hide')
+     nextButton.classList.remove('hide')
+    //  shuffledQuestions = questions.sort(() => Math.random() - .5)
+     currentQuestionIndex = 0
+     questionContainerElement.classList.remove('hide)')
+     setNextQuestion()
+ }
+
+ function setNextQuestion() {
+    if(currentQuestionIndex ==questionsList.length) {
+        alert("Congrats")
+        resetState()
+        clearStatusClass()
     }
+
+    showQuestion(questionsList[currentQuestionIndex])
+    currentQuestionIndex ++
+     clearStatusClass ()
+ }
+function showQuestion(question) {
+    questionElement.innerText = question.questionText
+    // question.answers.forEach(answer => {
+    //     const button =document.createElement('button')
+    //     button.innerText = answer.text
+    //     button.classList.add('btn')
+    //     if (answer.correct) {
+    //         button.dataset.correct = answer.correct
+    //     }
+    //     button.addEventListener('click', SelectAnswer)
+    //     answerButtonsElement.appendChild(button)
+    // })
+
 }
-// let question1 = new Question(true, "Is George Cool?")
-// console.log(question1)
-// console.log(question1.answer == false)
 
-let questionList =[new Question(true, "Airedale Terriers Served In World Wars"),
-new Question(false, "Airedale Terriers Are Primarily White"),
-new Question(true, "Airedale Terriers Were Bred To Hunt Feasants"),
-new Question(true, "Airedale Terriers Wore Gas Masks In The War"),
-new Question(true, "An Airedale named Bob Received A Victorian Medal"),
-new Question(false, "Airedale Terriers Are Known As The Golden Terrier")]
-for (let index = 0; index < questionList.length; index++) {
-console.log(questionList[index])
+function resetState() {
+    nextButton.classList.add('hide')
+    startButton.classList.remove('hide')
+    currentQuestionIndex = 0
+    }
 
-
-    
+function selectAnswer(userAnswer) {
+    currentQuestion = questionsList [currentQuestionIndex]
+    console.log(currentQuestion.answer== userAnswer)
+    setStatusClass(currentQuestion.answer)
 }
+function setStatusClass(actualAnswer) {
+    if(actualAnswer ==true){
+    trueButtonElement.classList.add("correct")
+    falseButtonElement.classList.add("wrong")
+    } else
+    {trueButtonElement.classList.add("wrong")
+    falseButtonElement.classList.add("correct")}
+}
+
+function clearStatusClass() {
+    trueButtonElement.classList.remove('correct')
+    trueButtonElement.classList.remove('wrong')
+    falseButtonElement.classList.remove('correct')
+    falseButtonElement.classList.remove('wrong')
+}
+
+let questionsList = [
+    {
+        questionText: 'Did Airedale Terriers Serve in The War?',
+        answer: true
+    },
+    {
+        questionText: 'Did Airedale Terriers Serve?',
+        answer: false
+    },
+    {
+        questionText: 'Did Airedale Terrien The War?',
+        answer: true
+    },
+
+
+]
+
